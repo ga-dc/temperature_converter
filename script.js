a62b45bde653f543f1dce50115a895edeb4ce00a
mwb86 <masonwbrown@gmail.com>
2016-11-20 10:39:46 -0500

    temp array works now

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 
-
-let temp = [];
-
-var temperature = prompt("what is the temperature");
+var STARTING_TEMP = prompt("what is the temperature");
 var temperature_unit = prompt("what is the temperature unit? F for farenheit, C for celsius, K for kelvin");
 
 if (temperature_unit == "F"){
- celsius = ((temperature -32)*5/9);
- kelvin = ((temperature -32)*5/9 +273 + "K");
- console.log("your kelvin temperature is " + kelvin);
- console.log("your celsius temperature is " + celsius);
+ let temp=[];
+ celsius = ((STARTING_TEMP -32)*5/9);
+ let CONVERTED_TEMP_1 = (celsius + "C");
+ kelvin = ((STARTING_TEMP -32)*5/9 +273);
+ let CONVERTED_TEMP_2 = (kelvin + "K");
+ temp.push(STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2);
+ console.log(temp);
+ //console.log("your kelvin temperature is " + kelvin);
+ //console.log("your celsius temperature is " + celsius);
  }
 else if (temperature_unit == "C"){
-  fahrenheit = ((temperature + 273));
-  kelvin = ((temperature * 9/5)+ 32);
-  console.log("your fahrenheit is " + fahrenheit);
-  console.log("your kelvin is " + kelvin);
+  let temp=[];
+  fahrenheit = ((STARTING_TEMP * 9/5)+ 32);
+  let CONVERTED_TEMP_1 = (fahrenheit + "F");
+  kelvin = ((STARTING_TEMP + 273));
+  let CONVERTED_TEMP_2 = (kelvin + "K");
+  temp.push(STARTING_TEMP,CONVERTED_TEMP_1,CONVERTED_TEMP_2);
+  console.log(temp);
 }
 else {
-  fahrenheit = ((temperature -273)*9/5 +32);
-  celsius = ((temperature - 273));
-  console.log("your fahrenheit is " + fahrenheit);
-  console.log("your celsius is " + celsius);
+  let temp=[];
+  fahrenheit = ((STARTING_TEMP -273)*9/5 +32);
+  let CONVERTED_TEMP_1=fahrenheit+"F";
+  celsius = ((STARTING_TEMP - 273));
+  let CONVERTED_TEMP_2=celsius+"K";
+  temp.push(STARTING_TEMP,CONVERTED_TEMP_1,CONVERTED_TEMP_2);
+  console.log(temp);
 }
