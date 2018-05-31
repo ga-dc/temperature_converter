8e63acb681f135713daadeceb3749e7d8c3f9799
nurikhwan <38243115+nurikhwan@users.noreply.github.com>
2018-05-31 08:24:08 +0800

    Add files via upload
    comfort level:3
    completeness level:2

---
+++ Part1/script.js
@@ -0,0 +1,4 @@
+var temperatureInFahr = prompt("What is the starting temperature(Fahrenheit)?"); {
+console.log("It is " + (( temperatureInFahr - 32 ) / ( 9 / 5 )) + "Celcius");
+console.log("It is " + (( temperatureInFahr + 459.67 ) * ( 5 / 9 )) + "Kelvin");
+};
