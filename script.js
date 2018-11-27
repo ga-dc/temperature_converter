cfe398d3e78625e2a2406ab475ab669e838cf769
Alan <alan88w@yahoo.com>
2018-11-27 17:11:24 +0800

    Part 1  Done

---
+++ script.js
@@ -0,0 +1,6 @@
+var temperatureInFahr = prompt("Please enter a temperature  in Fahrenheit: ");
+
+var temperatureInCelcius = (temperatureInFahr - 32) * 5 / 9;
+
+console.log("Temperature in Fahrenheit : ", temperatureInFahr);
+console.log("Temperature in Celcius : ", temperatureInCelcius);
